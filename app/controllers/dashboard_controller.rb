e053a5a1e32406fc8b204bae6ef315beaf2f0d10
Katarzyna Bernad <katarzyna.bernad@netguru.co>
2018-10-27 18:52:13 +0200

    usage of rails merge method and relations

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,12 @@ class DashboardController < ApplicationController
     # month = "to_char(#{extracted_month}, 'MM/YYYY')"
     @light_loads = LightLoad.group(extracted_month).order(extracted_month).count
     @match_percentage = { matched_loads: MatchedLoad.count, not_matched_loads: Load.count - MatchedLoad.count }
+
+    @drivers_with_big_cars = User.joins(:user_vehicles)
+                                 .merge(
+                                   UserVehicle.joins(:vehicle).merge(
+                                     Vehicle.where('vehicles.capacity > 10000')
+                                   )
+                                 )
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :jobs
   has_many :loads
   has_many :user_vehicles
+  has_many :big_vehicles, -> { where('capacity > 10000') }, through: :user_vehicles, source: :vehicle
 end
